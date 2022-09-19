b66dc7746ec5186de1c29dda606c153915642ae7
Frederik <git@caf-tranberg.dk>
2022-09-19 12:07:38 +0200

    updated folderes

---
+++ SendIP/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace UDPerSend
+{
+    class UDPerSend
+    {
+        const int PORT_NUMBER = 15000;
+
+        Thread t = null;
+        public void Start()
+        {
+            if (t != null)
+            {
+                throw new Exception("Already started, stop first");
+            }
+            Console.WriteLine("Started listening");
+            StartListening();
+        }
+        public void Stop()
+        {
+            try
+            {
+                udp.Close();
+                Console.WriteLine("Stopped listening");
+            }
+            catch { /* don't care */ }
+        }
+
+        private readonly UdpClient udp = new UdpClient(PORT_NUMBER);
+        IAsyncResult ar_ = null;
+
+        private void StartListening()
+        {
+            ar_ = udp.BeginReceive(Receive, new object());
+        }
+        private void Receive(IAsyncResult ar)
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, PORT_NUMBER);
+            byte[] bytes = udp.EndReceive(ar, ref ip);
+            string message = Encoding.ASCII.GetString(bytes);
+            Console.WriteLine("From {0} received: {1} ", ip.Address.ToString(), message);
+            StartListening();
+        }
+        public void Send(string message)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("255.255.255.255"), PORT_NUMBER);
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            client.Send(bytes, bytes.Length, ip);
+            client.Close();
+            Console.WriteLine("Sent: {0} ", message);
+        }
+
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+    }
+
+
+
+    class Program_s
+    {
+        static void Main(string[] args)
+        {
+            UDPerSend udp = new UDPerSend();
+            udp.Start();
+
+            do
+            {
+                Thread.Sleep(5000);
+                udp.Send(udp.GetLocalIPAddress());
+            } while (true);
+        }
+    }
+}
